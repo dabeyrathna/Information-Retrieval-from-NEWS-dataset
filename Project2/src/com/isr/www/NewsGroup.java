732656682912303cf36c6a7bce0b9a6cbfeba513
dabeyrathna <dabeyrathna@IST376-NUC02.unomaha.edu>
2016-10-29 19:52:44 -0500

    Max implemented, Added stop word list

--- Project2/src/com/isr/www/NewsGroup.java
+++ Project2/src/com/isr/www/NewsGroup.java
@@ -9,8 +9,9 @@ import java.util.Scanner;
 
 class Normalize{
 	
-	Map<String, ArrayList<String>> newsGroupMap = new HashMap<String, ArrayList<String>>();
-	Map<String,Integer> maxMap;
+	public Map<String, ArrayList<String>> newsGroupMap = new HashMap<String, ArrayList<String>>();
+	public Map<String,Integer> maxCountMap = new HashMap<String, Integer>();
+	public static String[] stopwords = {"a","articl", "as", "able", "about", "above", "according", "accordingly", "across", "actually", "after", "afterwards", "again", "against", "aint", "all", "allow", "allows", "almost", "alone", "along", "already", "also", "although", "always", "am", "among", "amongst", "an", "and", "another", "any", "anybody", "anyhow", "anyone", "anything", "anyway", "anyways", "anywhere", "apart", "appear", "appreciate", "appropriate", "are", "arent", "around", "as", "aside", "ask", "asking", "associated", "at", "available", "away", "awfully", "be", "became", "because", "become", "becomes", "becoming", "been", "before", "beforehand", "behind", "being", "believe", "below", "beside", "besides", "best", "better", "between", "beyond", "both", "brief", "but", "by", "cmon", "cs", "came", "can", "cant", "cannot", "cant", "cause", "causes", "certain", "certainly", "changes", "clearly", "co", "com", "come", "comes", "concerning", "consequently", "consider", "considering", "contain", "containing", "contains", "corresponding", "could", "couldnt", "course", "currently", "definitely", "described", "despite", "did", "didnt", "different", "do", "does", "doesnt", "doing", "dont", "done", "down", "downwards", "during", "each", "edu", "eg", "eight", "either", "else", "elsewhere", "enough", "entirely", "especially", "et", "etc", "even", "ever", "every", "everybody", "everyone", "everything", "everywhere", "ex", "exactly", "example", "except", "far", "few", "ff", "fifth", "first", "five", "followed", "following", "follows", "for", "former", "formerly", "forth", "four", "from", "further", "furthermore", "get", "gets", "getting", "given", "gives", "go", "goes", "going", "gone", "got", "gotten", "greetings", "had", "hadnt", "happens", "hardly", "has", "hasnt", "have", "havent", "having", "he", "hes", "hello", "help", "hence", "her", "here", "heres", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "hi", "him", "himself", "his", "hither", "hopefully", "how", "howbeit", "however", "i", "id", "ill", "im", "ive", "ie", "if", "ignored", "immediate", "in", "inasmuch", "inc", "indeed", "indicate", "indicated", "indicates", "inner", "insofar", "instead", "into", "inward", "is", "isnt", "it", "itd", "itll", "its", "its", "itself", "just", "keep", "keeps", "kept", "know", "knows", "known", "last", "lately", "later", "latter", "latterly", "least", "less", "lest", "let", "lets", "like", "liked", "likely", "little", "look", "looking", "looks", "ltd", "mainly", "many", "may", "maybe", "me", "mean", "meanwhile", "merely", "might", "more", "moreover", "most", "mostly", "much", "must", "my", "myself", "name", "namely", "nd", "near", "nearly", "necessary", "need", "needs", "neither", "never", "nevertheless", "new", "next", "nine", "no", "nobody", "non", "none", "noone", "nor", "normally", "not", "nothing", "novel", "now", "nowhere", "obviously", "of", "off", "often", "oh", "ok", "okay", "old", "on", "once", "one", "ones", "only", "onto", "or", "other", "others", "otherwise", "ought", "our", "ours", "ourselves", "out", "outside", "over", "overall", "own", "particular", "particularly", "per", "perhaps", "placed", "please", "plus", "possible", "presumably", "probably", "provides", "que", "quite", "qv", "rather", "rd", "re", "really", "reasonably", "regarding", "regardless", "regards", "relatively", "respectively", "right", "said", "same", "saw", "say", "saying", "says", "second", "secondly", "see", "seeing", "seem", "seemed", "seeming", "seems", "seen", "self", "selves", "sensible", "sent", "serious", "seriously", "seven", "several", "shall", "she", "should", "shouldnt", "since", "six", "so", "some", "somebody", "somehow", "someone", "something", "sometime", "sometimes", "somewhat", "somewhere", "soon", "sorry", "specified", "specify", "specifying", "still", "sub", "such", "sup", "sure", "ts", "take", "taken", "tell", "tends", "th", "than", "thank", "thanks", "thanx", "that", "thats", "thats", "the", "their", "theirs", "them", "themselves", "then", "thence", "there", "theres", "thereafter", "thereby", "therefore", "therein", "theres", "thereupon", "these", "they", "theyd", "theyll", "theyre", "theyve", "think", "third", "this", "thorough", "thoroughly", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "took", "toward", "towards", "tried", "tries", "truly", "try", "trying", "twice", "two", "un", "under", "unfortunately", "unless", "unlikely", "until", "unto", "up", "upon", "us", "use", "used", "useful", "uses", "using", "usually", "value", "various", "very", "via", "viz", "vs", "want", "wants", "was", "wasnt", "way", "we", "wed", "well", "were", "weve", "welcome", "well", "went", "were", "werent", "what", "whats", "whatever", "when", "whence", "whenever", "where", "wheres", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whos", "whoever", "whole", "whom", "whose", "why", "will", "willing", "wish", "with", "within", "without", "wont", "wonder", "would", "would", "wouldnt", "yes", "yet", "you", "youd", "youll", "youre", "youve", "your", "yours", "yourself", "yourselves", "zero"};
 
 	public Normalize(String fileName) {
 		readDataset(fileName);
@@ -78,16 +79,50 @@ class Normalize{
 	}
 	
 	public void constructMaxFrequecyMap(){
-		maxMap = new HashMap<String, Integer>();
+		Map<String,Integer> maxMap = new HashMap<String, Integer>();
+
+		for (Map.Entry<String,ArrayList<String>> entry : newsGroupMap.entrySet()) {
+			for (String l : entry.getValue()) {
+				nextWord: for (String wordSet : l.split("\\s")) {
+					String temp = wordSet.toLowerCase();
+					for (String w : stopwords) {
+						if(w.equals(temp)) 
+							break nextWord;
+					}
+					if(maxMap.containsKey(temp))
+						maxMap.put(temp, (maxMap.get(temp) + 1));
+					else
+						maxMap.put(temp, 1); 
+				}
+			}
+			System.out.print("News group is :  "+ entry.getKey()+" ==== ");
+			maxCountMap.put(entry.getKey(), getMax(maxMap));
+			maxMap.clear();
+		}
+		System.out.println(maxCountMap.size());
+	}
+	
+	public int getMax(Map<String, Integer> maxMap) {
+		int max = 0;
+		int temp = 0;
+		String m ="";
+		for (Map.Entry<String, Integer> entry : maxMap.entrySet())
+		{
+			temp = entry.getValue();                          ////////////// we can use this method do find most frequent words in several newsgroup. for now just max 
+		    if (temp > max){
+		        max = temp;
+		        m = entry.getKey();
+		    }
+		}
+		System.out.println(m + " ====   "+max);
+		return max;
 	}
 }
 
 public class NewsGroup {
 
 	public static void main(String[] args) {
-
 		Normalize normalize = new Normalize("newsGroups.txt"); 
-		normalize.checkWordCountVariations();
 	}
 
 }
